858172bb4a685888011232a4a3c707cbdd8bcdfb
leen-duafi <leen-hala@hotmail.com>
2021-08-03 16:31:15 +0300

    lab 07 done

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,43 @@
 const express = require('express') // require the express package ==> import React from 'react'
 require('dotenv').config();
-require('./data/weather.json')
+let data = require('./data/weather.json')
 const cors = require('cors');
 const app = express() // initialize your express app instance
-
-app.get('/weather', // our endpoint name
-    (request, responess) => { // callback function of what we should do with our request
-      responess.status(500).weather();
-      // responess.send('Hello Weather')
-    })
-
-    app.get('/dogs/:id', (req, res) => {
-      let id = req.params.id;
-      let response = weather.find((item) => item.id == id);
-      
-      res.status(200).json(response);
-    });
 
 app.use(cors())
 const PORT = process.env.PORT;
 
+class Forecast {
+  constructor(value) {
+    this.valid_date = value.valid_date;
+    this.description = value.weather.description;
+  }
+}
 // a server endpoint 
 app.get('/', // our endpoint name
-    (req, responess) => { // callback function of what we should do with our request
-      responess.send('Hello World') // our endpoint function response
-    })
+  (req, responess) => { // callback function of what we should do with our request
+    responess.send('Hello World') // our endpoint function response
+  })
 
-    
+app.get('/weather', (req, res) => {
+
+  try {
+    let { searchQuery, lat, lon } = req.query;
+    let weatherDta = data.find(element =>
+      element.city_name.toLowerCase() === searchQuery.toLowerCase() ||
+      (element.lat === lat && element.lon === lon)
+    );
+    let forecastArr = weatherDta.data.map(items => { 
+      console.log(items)
+      return  new Forecast(items)});
+    res.send(forecastArr);
+  }
+  catch (e) {
+    res.status(404).send('No Data for this City');
+  }
+});
 
 // app.listen(3000) // kick start the express server to work
 app.listen(PORT, () => {
-    console.log(`this is me ${PORT}`,require('./data/weather.json'))
+  console.log(`this is me ${PORT}`, require('./data/weather.json'))
 })
